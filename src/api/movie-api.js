b6311d5efd9c572097cbc69b5bffb56e26a54398
facuschoenfeld <facuschoenfeld@gmail.com>
2025-09-22 20:06:18 -0300

    update api 2

--- src/api/movie-api.js
+++ src/api/movie-api.js
@@ -13,7 +13,13 @@ const options = {
 
 export async function getMovies() {
     try {
-        const res = await fetch(`${API_URL}?api_key=${API_KEY}&language=es-ES&page=1`)
+        const res = await fetch(`${API_URL}?api_key=${API_KEY}&language=es-ES&page=1`,{
+            method: 'GET',
+            headers:{
+                accept: 'application/json',
+                Authorization: `Bearer ${API_TOKEN}`
+            },
+        })
         if(!res.ok) throw new Error("Error al cargar películas")
         const data = await res.json()
         return data.results
@@ -26,7 +32,7 @@ export async function getMovies() {
 
 export async function searchMovies(query) {
     if(!query) return []
-    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${query}`
+    const searchUrl = `https://api.themoviedb.org/3/search/movie?query=${query}&language=es-ES`
     try {
         const res = await fetch(searchUrl, options)
         if(!res.ok) throw new Error("Error al buscar película")
